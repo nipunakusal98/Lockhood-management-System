7eafba23e21d38dca97313eff0d1ea99eea71b5e
nipunakusal98 <nipuna.suraweera98@gmail.com>
2022-12-08 23:23:53 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -5,22 +5,87 @@ import Users from './components/Users/users';
 import Factory from './components/Factory/factory';
 import Department from './components/Department/department';
 import WorkshopUnit from './components/WorkshopUnit/workshop_unit';
+import IssueHistory from './components/Issue History/issue_history';
+import KanbanCard from './components/Kanban card/kanban_card';
+import Material from './components/Material/material';
+import MaterialIssueHistory from './components/Material Issue History/material_issue_history';
+import MaterialList from './components/Material List/material_list';
+import Warehouse from './components/Warehouse/warehouse';
+import WarehouseHistory from './components/Warehouse History/warehouse_history';
+import Drawer from './components/SideBar/drawer';
+import Home from './home';
 
 
-function App() {
-  return (
-    <div className="App">
+// import ResponsiveAppBar from './components/NavBar/nav';
+import { BrowserRouter as Router,Route, Routes} from "react-router-dom";
+import { makeStyles } from '@mui/material';
+
+
+
+
+
+
+export default function App() {
+   return (
+   
+      <div className='App'>
+
+        <Drawer/>
+
+        <Router>
+          <Routes>
+          
+            <Route path="/home" element={<Home />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/employee" element={<Users />}/>
+            <Route path="/factory" element={<Factory />}/>
+            <Route path="/employee_type" element={<EmployeeType />}/>
+            <Route path="/dep" element={<Department />}/>
+            <Route path="/w_unit" element={<WorkshopUnit />}/>
+            <Route path="/issue_history" element={<IssueHistory />}/>
+            <Route path="/kanban" element={<KanbanCard />}/>
+            <Route path="/material" element={<Material />}/>
+            <Route path="/material_issue" element={<MaterialIssueHistory />}/>
+            <Route path="/Material_list" element={<MaterialList />}/>
+            <Route path="/warehouse" element={<Warehouse />}/>
+            <Route path="/warehouse_history" element={<WarehouseHistory />}/>
+            
+          </Routes>
+        </Router>
+
+
+      </div>
       
-      {/* <Login/> */}
-      {/* <Users/> */}
-      {/* <EmployeeType/> */}
-      {/* <Factory/> */}
+   
+    
 
-      {/* <Department/> */}
-      <WorkshopUnit/>
-      
-    </div>
   );
 }
+ 
 
-export default App;
+   
+      {/* <Drawer/> */}
+
+      
+        {/* <Route exact from="/employees" render ={props =><Users {...props}/>}/>
+        <Route exact from="/department" render ={props =><Department {...props}/>}/>
+        <Route exact from="/login" render ={props =><Login {...props}/>}/>
+        <Route exact from="/employee_type" render ={props =><EmployeeType {...props}/>}/>
+        <Route exact from="/factory" render ={props =><Factory {...props}/>}/>
+        <Route exact from="/kanban" render ={props =><KanbanCard {...props}/>}/>
+        <Route exact from="/material" render ={props =><Material {...props}/>}/>
+        <Route exact from="/material_issue_history" render ={props =><MaterialIssueHistory {...props}/>}/>
+        <Route exact from="/material_list" render ={props =><MaterialList {...props}/>}/>
+        <Route exact from="/warehouse" render ={props =><Warehouse {...props}/>}/>
+        <Route exact from="/warehouse_history" render ={props =><WarehouseHistory {...props}/>}/>
+        <Route exact from="/users" render ={props =><WorkshopUnit {...props}/>}/>
+        <Route exact from="/users" render ={props =><IssueHistory {...props}/>}/> */}
+
+
+        
+      
+   
+
+      
+  
+
---
+++ src/components/SideBar/drawer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Drawer as MUIDrawer} from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+
+
+
+const Drawer = () =>{
+    return(
+        <MUIDrawer variant="permanent">
+            <List>
+
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                {/* <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon> */}
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+                
+            </List>
+        </MUIDrawer>
+        
+    )
+}
+export default Drawer;
